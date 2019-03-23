d227bdd442ed36a65b71566856905d4f4786fa69
ranjan mondal <ranjan.mondal@oodlestechnologies.com>
2019-03-23 15:17:54 +0530

    Junit: Assumptions, Displayname and Disabled

--- src/main/java/com/trader/utils/MathUtils.java
+++ src/main/java/com/trader/utils/MathUtils.java
@@ -2,7 +2,7 @@ package com.trader.utils;
 
 public class MathUtils {
 
-    public static int add(int a, int b){
+    public int add(int a, int b){
         return a + b;
     }
 
--- src/test/java/com/trader/utils/MathUtilsTest.java
+++ src/test/java/com/trader/utils/MathUtilsTest.java
@@ -1,21 +1,35 @@
 package com.trader.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
 
+    MathUtils mathUtils;
+
+    @BeforeAll
+    void init(){
+        mathUtils = new MathUtils();
+    }
+
     @Test
+    @DisplayName("Addtion of two numbers")
     void add() {
         int expected = 2;
-        int actual = MathUtils.add(1, 1);
+        int actual = mathUtils.add(1, 1);
         assertEquals(expected, actual);
     }
 
     @Test
+    @DisplayName("Division of two numbers")
+    //@Disabled
     void divide() {
-        MathUtils mathUtils = new MathUtils();
-        assertThrows(ArithmeticException.class,() -> mathUtils.divide(1, 0), "Exception not found");
+        boolean isServerUp = false;
+       // System.out.println(System.getProperty("ENV"));
+        Assumptions.assumeTrue(isServerUp);
+        assertThrows(ArithmeticException.class,() -> mathUtils.divide(1, 0),
+                "Exception not found");
     }
 }
